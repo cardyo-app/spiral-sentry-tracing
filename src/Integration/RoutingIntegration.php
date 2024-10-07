02decdbc413a70700be6d64592ae18bd69e33614
Leonid Meleshin <hello@leon0399.ru>
2024-10-07 22:14:54 +0200

    ✨ Trace routing span

--- src/Integration/RoutingIntegration.php
+++ src/Integration/RoutingIntegration.php
@@ -4,13 +4,20 @@ namespace Cardyo\SpiralSentryTracing\Integration;
 
 use Sentry\Integration\IntegrationInterface;
 use Sentry\SentrySdk;
+use Sentry\Tracing\Span;
+use Sentry\Tracing\SpanContext;
 use Sentry\Tracing\TransactionSource;
 use Spiral\Events\ListenerRegistryInterface;
 use Spiral\Router\Event\RouteMatched;
+use Spiral\Router\Event\RouteNotFound;
+use Spiral\Router\Event\Routing;
 use Spiral\Router\Router;
 
 class RoutingIntegration implements IntegrationInterface
 {
+    private ?Span $parentSpan = null;
+    private ?Span $span = null;
+
     public function __construct(
         protected readonly ListenerRegistryInterface $listenerRegistry
     ) {
@@ -22,10 +29,33 @@ class RoutingIntegration implements IntegrationInterface
             return;
         }
 
+        $this->listenerRegistry->addListener(Routing::class, [$this, 'onRouting']);
         $this->listenerRegistry->addListener(RouteMatched::class, [$this, 'onRouteMatched']);
+        $this->listenerRegistry->addListener(RouteNotFound::class, [$this, 'onRouteNotFound']);
+    }
+
+    public function onRouting(Routing $event): void
+    {
+        $this->parentSpan = SentrySdk::getCurrentHub()->getSpan();
+        if ($this->parentSpan === null) {
+            return;
+        }
+
+        $this->span = $this->parentSpan->startChild(SpanContext::make()
+            ->setOp('routing')
+            ->setDescription($event->request->getUri()->getPath())
+        );
+
+        SentrySdk::getCurrentHub()->setSpan($this->span);
     }
 
     public function onRouteMatched(RouteMatched $event): void
+    {
+        $this->updateTransactionName($event);
+        $this->finishRoutingSpan();
+    }
+
+    protected function updateTransactionName(RouteMatched $event): void
     {
         $transaction = SentrySdk::getCurrentHub()->getTransaction();
 
@@ -52,6 +82,24 @@ class RoutingIntegration implements IntegrationInterface
                 'route' => $routeName,
             ]);
         }
+    }
+
+    public function onRouteNotFound(RouteNotFound $event): void
+    {
+        $this->finishRoutingSpan();
+    }
+
+    protected function finishRoutingSpan(): void
+    {
+        if ($this->span === null) {
+            return;
+        }
+
+        $this->span->finish();
+        $this->span = null;
+
+        SentrySdk::getCurrentHub()->setSpan($this->parentSpan);
+        $this->parentSpan = null;
     }
 
     public function isEnabled(): bool
